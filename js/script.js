32c24a7e43e304ae9a772fef61a1d447ae77ada4
albertomandirola <albymandi00@gmail.com>
2023-12-01 16:24:10 +0100

    [UPDATE]add function in script.js

--- js/script.js
+++ js/script.js
@@ -7,7 +7,36 @@ function generateRandomNumber(){
             arrey.push(number)
         }
     }
-    console.log(arrey)
-
+    
     return arrey;
 }
+//CREATE FUNCTION THAT PERMISS TO INSERT NUMBER BY PROMPT 
+function insertPlayerNumber(){
+    let arrey_player = []
+    while(arrey_player.length != 5){
+        let number = parseInt(prompt('inserire un numero'))
+        if(!arrey_player.includes(number)){
+            arrey_player.push(number)
+        }
+        else{
+            alert('hai gia inserito questo numero')
+        }
+    }
+    return arrey_player
+    
+}
+let arrey_player = []    
+let arrey_pc = generateRandomNumber()
+document.getElementById('numbers').innerText += arrey_pc
+
+
+setTimeout(function(){
+    document.getElementById('numbers').innerText = ''
+},3000)
+
+setTimeout(function(){
+    arrey_player = insertPlayerNumber()
+    document.getElementById('numbers_player').innerText = arrey_player
+    document.getElementById('numbers').innerText += arrey_pc
+
+},5000)
